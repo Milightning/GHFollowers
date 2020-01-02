e585bb0e753eed0306db2c62127ecc025ea410f8
Kate Chirkina <milightning.ua@gmail.com>
2020-01-02 22:52:19 +0200

    Add FavoritesListVC and SearchVC classes

--- GHFollowers/FavoritesListVC.swift
+++ GHFollowers/FavoritesListVC.swift
@@ -1,21 +1,19 @@
 //
-//  ViewController.swift
+//  FavoritesListVC.swift
 //  GHFollowers
 //
-//  Created by Catherine on 01.01.2020.
+//  Created by Catherine on 02.01.2020.
 //  Copyright © 2020 Catherine Priazhnikova. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoritesListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
-        
+
+        view.backgroundColor = .systemTeal
     }
-
-
+    
 }
-
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,19 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Catherine on 02.01.2020.
+//  Copyright © 2020 Catherine Priazhnikova. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemRed
+    }
+
+}
